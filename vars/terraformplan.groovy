de614ba7d93a2131f906944c970aa36a10720411
shaktikumar1987 <108357872+shaktikumar1987@users.noreply.github.com>
2024-02-24 12:26:36 +0400

    Update terraformplan.groovy

--- vars/terraformplan.groovy
+++ vars/terraformplan.groovy
@@ -1,23 +1,6 @@
 #!/usr/bin/env groovy
 
-package io.terraformbuild
+def call(String folderpath='tfcode') {
 
-class terraformClass {
-
-    String folderpath
-    static String stage = "init"
-    static String stage = "validate"
-    static String stage = "fmt"
-    static String stage = "plan"
-    String msg
-
-
-    def tfinit(){
-
-            this.msg = "This is my tf init code from folder ${folderpath}"
-
-
-    }
-
-
+    echo "Hello, ${folderpath}."
 }
