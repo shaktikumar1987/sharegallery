3d29a5c0c89b75e852ab27bed6ca1945ab603bcf
shaktikumar1987 <108357872+shaktikumar1987@users.noreply.github.com>
2024-02-24 13:10:24 +0400

    Update terraformplan.groovy

--- vars/terraformplan.groovy
+++ vars/terraformplan.groovy
@@ -1,13 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String folderpath='tfcode') {
-
-     echo "Hello, ${folderpath}."
-   // powershell 'write-host " this is  jenkins example ${$env:folderpath}"'
-    //powershell write-host "this is  jenkins example $($env:folderpath)"
-    //powershell 'write-host " this is  jenkins example $($env:folderpath)"'
-
-$folderpath = "$(ENV:folderpath)"
-Write-Host "This is jenkins Example, $folderpath."
-'''
+powershell 'def call(String folderpath=\'tfcode\') {'
+  powershell 'write-host " this is  jenkins example ${$env:folderpath}"'
 }
